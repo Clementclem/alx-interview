f9c8fc3674b46383b55565bb3dd48b3753386306
Clem <109198546+Clementclem@users.noreply.github.com>
2023-06-29 16:45:39 +0300

    Update 0-pascal_triangle.py

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,36 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""returns a pascal triangle"""
-
-
-def pascal_triangle(n):
-    """
-    returns a list of integers in a pascal triangle format
-    """
-
-    if n <= 0:
-        # return empty list
-        return []
-    pascal = [[1]]
-    if n == 1:
-        return pascal
-
-    for i in range(1, n):
-        left = -1
-        right = 0
-        in_pas = []
-        for j in range(i + 1):
-            num = 0 
-            if left > -1:
-                num += pascal[i - 1][left]
-            if right < i:
-                num += pascal[i - 1][right]
-            left += 1
-            right += 1
-            in_pas.append(num)
-        pascal.append(in_pas)
-    return pascal
-=======
 """
 0. Pascal's Triangle
 """
@@ -50,4 +18,3 @@ def pascal_triangle(n):
                 C = C * (i - j) // j
             res.append(level)
     return res
->>>>>>> c46a1f3487e32d08e94d7a0aa37923ea8f7be2d5
